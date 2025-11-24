becf98cc4d9fbe171f165b30567259862d3749b5
utkarsh25bce10695 <utkarsh.25bce10695@vitbhopal.ac.in>
2025-11-24 09:39:01 +0530

    Update message_encrypt_decrypt_app.py

--- message_encrypt_decrypt_app.py
+++ message_encrypt_decrypt_app.py
@@ -1,1 +1,46 @@
 
+# message encrypt
+print(" \n===== WELCOME TO THE MESSAGE ENCRYPT DECRYPT. ===== ")
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "utkarsh" and password == "utkarsh@123":
+    import string
+  
+    def caesar_encrypt(message, key):
+        shift = key % 26
+        cipher = str.maketrans(
+            string.ascii_lowercase,
+            string.ascii_lowercase[shift:] + string.ascii_lowercase[:shift]
+        )
+        encrypted_message = message.lower().translate(cipher)
+        return encrypted_message
+
+    def caesar_decrypt(encrypted_message, key):
+        shift = 26 - (key % 26)
+        cipher = str.maketrans(
+            string.ascii_lowercase,
+            string.ascii_lowercase[shift:] + string.ascii_lowercase[:shift]
+        )
+        message = encrypted_message.translate(cipher)
+        return message
+
+    while True:
+        message = input("Write a message: ").lower()
+        choice = input("Do you want to decrypt or encrypt? ").lower()
+        key = int(input("How much shift do you want? "))
+
+        if choice == "encrypt":
+            print("Encrypted message:", caesar_encrypt(message, key))
+        elif choice == "decrypt":
+            print("Decrypted message:", caesar_decrypt(message, key))
+        else:
+            print("Please type encrypt or decrypt.")
+
+        again = input("Do you want to do it with another message? (yes/no): ").lower()
+        if again != "yes":
+            print("Thanks for using this game\nExiting....")
+            break
+else:
+    print("Wrong Username or Password...Access denied.")
